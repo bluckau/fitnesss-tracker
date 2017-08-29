0ccc675b5715392c21be089e97b11d6e8adc52cd
Brian Luckau <Brian Luckau>
2017-08-29 16:13:07 -0600

    added a controller

---
+++ src/main/java/com/pluralsight/controller/HelloController.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.pluralsight.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author bluck
+ *
+ */
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = "/greeting")
+	public String sayHello(Model model) {
+		model.addAttribute("greeting", "Hello World");
+		return "hello";
+	}
+
+}
---
+++ src/main/java/com/pluralsight/model/Exercise.java
@@ -0,0 +1,14 @@
+package com.pluralsight.model;
+
+public class Exercise {
+
+	private int minutes;
+
+	public int getMinutes() {
+		return this.minutes;
+	}
+
+	public void setMinutes(int minutes) {
+		this.minutes = minutes;
+	}
+}
